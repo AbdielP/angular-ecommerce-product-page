b614d95f3f258e38b1877a4fc4ee09f722a7f910
AbdielP <abdiel.pinzonc@gmail.com>
2021-11-09 22:04:16 -0500

    variable names changed. working on thumbnail and desktop gallery

--- src/app/components/gallery/gallery.component.ts
+++ src/app/components/gallery/gallery.component.ts
@@ -9,33 +9,34 @@ import { GalleryService } from 'src/app/services/gallery.service';
 export class GalleryComponent implements OnInit {
 
   gallery: Array<any> = [];
-  actualImage: string = '';
-  actualIndex: number = 0;
+  currentImage: string = '';
+  currentThumb: string = '';
+  currentIndex: number = 0;
 
   constructor(private galleryService: GalleryService) { }
 
   ngOnInit(): void {
     this.gallery = this.galleryService.getGallery();
-    this.actualImage = this.gallery[0].img;
+    this.currentImage = this.gallery[0].img;
     console.log(this.gallery);
   }
 
   galleryForward(): void {
-    if (this.actualIndex < this.gallery.length -1) {
-      this.actualIndex++;
-      let nextImage = this.gallery[this.actualIndex].img;
-      this.actualImage = nextImage;
+    if (this.currentIndex < this.gallery.length -1) {
+      this.currentIndex++;
+      let nextImage = this.gallery[this.currentIndex].img;
+      this.currentImage = nextImage;
     } else {
-      this.actualIndex = 0;
-      this.actualImage = this.gallery[0].img;
+      this.currentIndex = 0;
+      this.currentImage = this.gallery[0].img;
     }
   }
 
   galleryBackward(): void {
-    if (this.actualIndex > 0) {
-      this.actualIndex--;
-      let prevImage = this.gallery[this.actualIndex].img;
-      this.actualImage = prevImage;
+    if (this.currentIndex > 0) {
+      this.currentIndex--;
+      let prevImage = this.gallery[this.currentIndex].img;
+      this.currentImage = prevImage;
     }
   }
 
