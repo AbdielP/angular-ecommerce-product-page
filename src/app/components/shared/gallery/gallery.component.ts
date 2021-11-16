912be03519ebe1ea79252b7a2815d9d76cda2366
AbdielP <abdiel.pinzonc@gmail.com>
2021-11-16 02:19:13 -0500

    lightbox thumnails size fixed

--- src/app/components/shared/gallery/gallery.component.ts
+++ src/app/components/shared/gallery/gallery.component.ts
@@ -57,7 +57,7 @@ export class GalleryComponent {
     if( this.callLightbox && this.width >= 550 ) {
       this.dialog.open(LightboxComponent, {
         data: { gallery: this.gallery },
-        panelClass: ['gallery__img--resize', 'thumbnails--resize']
+        panelClass: ['gallery__img--resize', 'thumbnails--resize', 'test']
       });
     }
   }
