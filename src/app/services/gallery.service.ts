96c5a34f96a2a8b96a39fb692fed697d9c8ccdc6
AbdielP <abdiel.pinzonc@gmail.com>
2021-11-25 14:14:17 -0500

    assets route changed

--- src/app/services/gallery.service.ts
+++ src/app/services/gallery.service.ts
@@ -8,10 +8,10 @@ export class GalleryService {
   imgIndex: number = 1;
 
   gallery: Array<Object> = [
-    { 'img': '/assets/image-product-1.jpg', 'thumb': '/assets/image-product-1-thumbnail.jpg' },
-    { 'img': '/assets/image-product-2.jpg', 'thumb': '/assets/image-product-2-thumbnail.jpg' },
-    { 'img': '/assets/image-product-3.jpg', 'thumb': '/assets/image-product-3-thumbnail.jpg' },
-    { 'img': '/assets/image-product-4.jpg', 'thumb': '/assets/image-product-4-thumbnail.jpg' }
+    { 'img': 'assets/image-product-1.jpg', 'thumb': 'assets/image-product-1-thumbnail.jpg' },
+    { 'img': 'assets/image-product-2.jpg', 'thumb': 'assets/image-product-2-thumbnail.jpg' },
+    { 'img': 'assets/image-product-3.jpg', 'thumb': 'assets/image-product-3-thumbnail.jpg' },
+    { 'img': 'assets/image-product-4.jpg', 'thumb': 'assets/image-product-4-thumbnail.jpg' }
   ]
 
   getGallery(): Array<Object> {
